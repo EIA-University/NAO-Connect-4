9e0fcd36e9d854fd1977983028ff8623cf0803aa
Diego Alejandro Oquendo Castaño <30699018+DiegoOquendo@users.noreply.github.com>
2019-10-04 08:25:44 -0500

    Migrating to OpenCV

--- __main__.py
+++ __main__.py
@@ -4,29 +4,44 @@ import convertirImgToMatrix as conv
 #import time
 #from naoqi import ALProxy
 #import Fourinline as games
-#desktop-keel9jm.local.:54103
-#nao.local 9559
-# if __name__ == '__nomain__':
+# desktop-keel9jm.local.:54103
+# nao.local 9559
+
+# Main para utilizar con NAO
+# if __name__ == '__main__':
 #     IP = "nao.local"  # Replace here with your NaoQi's IP address.
 #     PORT = 9559
 #     while True:
-#         path = take.sinNAO(IP, PORT)
-#         #path = "imagenes/foto.jpg"
+#         path = take.showNaoImage(IP, PORT)
 #         print path
 #         matrix = conv.ejecutar(path)
 #         print matrix
-#         if matrix != False:
-#             jugada = games.play(matrix)
-#             talk.talkSinNAO("Put the piece in the colum " + str(jugada), IP, PORT)
-#             talk.talkSinNAO("It is your turn", IP, PORT)
-#             time.sleep(10)
-#         else:
-#             talk.talkSinNAO("I can't see right the game", IP, PORT)
-#             time.sleep(5)
+#         jugada = games.play(matrix)
+#         talk.talk("Put the piece in the colum " + str(jugada), IP, PORT)
+#         time.sleep(1)
+#         talk.talk("It's your turn, in 10 seconds, i'm going to play", IP, PORT)
+#         time.sleep(10)
 
 if __name__ == '__main__':
-    path = "imagenes/prueba5.jpg" 
-    #path = "imagenes/pruebaNao1.png" # Las fotos de Nao son .png
-    print path
+    path = "Utilizar/ldowaaumyy.png"
     matrix = conv.ejecutar(path)
-    print matrix
+    # path1 = "editadas/cuadricula"
+    # stri = "imagenes/prueba9.png"
+    # matrix = conv.ejecutar(stri)
+    # matrix.save(path1)
+
+    # for i in range(1,20):
+    #     stri = "imagenes/prueba"
+    #     path1 = "cuadricula/prueba"
+    #     stri = stri + str(i)
+    #     stri = stri + ".png"
+    #     path1 = path1 + str(i)
+    #     path1 = path1 + ".png"
+    #     path = stri
+
+    #     print path
+    #     matrix = conv.ejecutar(path)
+        # matrix.save(path1)
+    # print path
+    # matrix = conv.ejecutar(path)
+    #path = "imagenes/pruebaNao1.png" # Las fotos de Nao son .png
--- convertirImgToMatrix.py
+++ convertirImgToMatrix.py
@@ -1,40 +1,47 @@
 from PIL import Image
 import math
 import PIL
+from PIL import ImageDraw
+from PIL import ImageFilter
+import numpy as np
+import cv2
 
 # Espacio: 0
 # Fichas blancas (IA): 1
 # Fichas Negras (Enemigo): -1
 
-def isBlack(r, g, b):
-    if r < 110 and r > 15 and g < 80 and g > 40 and b < 110 and b > 60:
-        return True
-    return False
+# Retorna el numero del color identificado: 0 Negro, 1 Blanco, 2 Verde
+def getColor(color):
+    # Valores malechores ;)
+    avgBlack = 63.30769231
+    avgWhite = 210.5897436
+    avgGreen = 113.8717949
+    
 
-def isWhite(r, g, b):
-    if r > 140 and g > 220 and b > 220:
-        return True
-    return False
+    c = color[0] + color[1] + color[2]
+    c = c / 3
 
-def isRed(r, g, b):
-    if r < 200 and r >= 90 and g < 110 and g > 50 and b < 110 and b > 45:
-        return True
-    return False
+    distancias = []
+    distancias.append(abs(c - avgBlack))
+    distancias.append(abs(c - avgWhite))
+    distancias.append(abs(c - avgGreen))
 
-def isFondo(r, g, b):
-    if r > 25 and r < 50 and g > 145 and g < 215 and b > 128 and b < 195:
-        return True
-    return False
+    mini = min(distancias)
+    i = 0
+    while i < 3:
+        if distancias[i] == mini:
+            return i
+        i += 1
 
 # Retorna la posicion del pixel inicial y final en donde se encuentra el tablero (Verticalmente)
 def delimitarAlto(img, w, h):
     posIni = -1
     posFin = -1
-    middle = math.trunc(w/2)
+    middle = math.trunc(w / 2)
     cont = 0 # 3 pixeles consecutivos seran la sennal
     i = 0
     stop = False # Indicacion de parada
-    while (i<h and (not stop)): # Buscar la posicion inicial
+    while (i < h and (not stop)): # Buscar la posicion inicial
         c = img.getpixel((middle, i))
         if c == 0: # Si es negro
             cont += 1
@@ -47,7 +54,7 @@ def delimitarAlto(img, w, h):
     cont = 0
     i = h-1
     stop = False
-    while (not stop): # Buscar la posicion final
+    while ( i > 0 and (not stop)): # Buscar la posicion final
         c = img.getpixel((middle, i))
         if c == 0: # Si es negro
             cont += 1
@@ -57,7 +64,7 @@ def delimitarAlto(img, w, h):
             posFin = i
             stop = True
         i -= 1 # Avanzar en el ciclo
-    return(posIni, posFin)
+    return(posIni - 10, posFin - 14)
 
 # Retorna la posicion del pixel inicial y final en donde se encuentra el tablero (Horizontalmente)
 def delimitarAncho(img, w, h):
@@ -67,7 +74,7 @@ def delimitarAncho(img, w, h):
     cont = 0 # 3 pixeles consecutivos seran la sennal
     i = 0
     stop = False # Indicacion de parada
-    while (i<w and (not stop)): # Buscar la posicion inicial
+    while (i < w and (not stop)): # Buscar la posicion inicial
         c = img.getpixel((i, middle))
         if c == 0: # Si es negro
             cont += 1
@@ -80,7 +87,7 @@ def delimitarAncho(img, w, h):
     cont = 0
     i = w-1
     stop = False
-    while (not stop): # Buscar la posicion final
+    while (i > 0 and (not stop)): # Buscar la posicion final
         c = img.getpixel((i, middle))
         if c == 0: # Si es negro
             cont += 1
@@ -90,130 +97,138 @@ def delimitarAncho(img, w, h):
             posFin = i
             stop = True
         i -= 1 # Avanzar en el ciclo
-    return(posIni, posFin)
+    return(posIni + 10, posFin)
 
-def getStates(img2, L):
-    #Variables necesarias
-    countR = 0
-    countB = 0
-    countW = 0
-    countE = 0
-    flagE = False
-    flagW = False
-    flagB = False
-    flagR = False
-    flagF = False
-    v = []
-    i = 100
-    k = 0
-    #Recorro la imagen
-    while i < 640:
-        (r,g,b) = img2.getpixel((i, L))
-        #Determino si ya estoy en la parte roja del tablero
-        if flagR is not True:
-            if isRed(r,g,b):
-                countR = countR + 1
-            if countR > 5:
-                countR = 0
-                flagR = True
-        else:
-            if flagF is not True:
-                if isRed(r,g,b) is not True:
-                    if isFondo(r,g,b):
-                        countE = countE + 1
-                    elif isBlack(r,g,b):
-                        countB = countB + 1
-                    elif isWhite(r,g,b):
-                        countW = countW + 1 
-                    if countE > 5:
-                        flagE = True
-                        flagF = True
-                    elif countB > 5:
-                        flagB = True
-                        flagF = True
-                    elif countW > 5:
-                        flagW = True
-                        flagF = True
-            else:
-                if k <= 6:
-                    if flagB:
-                        v.append(-1)
-                        flagB = False
-                        flagF = False
-                        flagR = False
-                        countB = 0
-                        k = k + 1
-                    elif flagW:
-                        v.append(1)
-                        flagW = False
-                        flagF = False
-                        flagR = False
-                        countW = 0
-                        k = k + 1
-                    elif flagE:
-                        v.append(0)
-                        flagE = False
-                        flagF = False
-                        flagR = False
-                        countE = 0
-                        k = k + 1
-        i = i + 1
-    return v
-    
-def createMatrix(V1, V2, V3, V4, V5, V6):
-    mat = [V1, V2, V3, V4, V5, V6]
-    #print len(mat)
-    return mat
+# Retorna el tipo de estado de la ficha, segun el color del centro de su delimitacion
+def getPoints(img, w, h):
+    # Sacamos la mitad de los cuadros
+    ySpace = math.trunc(w / 7) # n
+    yCenter = math.trunc(ySpace / 2) # m
+    xSpace = math.trunc(h / 6) 
+    xCenter = math.trunc(xSpace / 2)
+    # Matriz de puntos de los centros
+    puntos = []
+    for i in range(1, 15, 2):
+        p = []
+        for j in range(1, 13, 2):
+            p.append((yCenter * i, xCenter * j))
+        puntos.append(p)
+    return puntos
 
-def validarMatrix(matrix):
-    for i in range(0,len(matrix) - 1):
-        if len(matrix[i]) < 7:
-            return False
-    return matrix
+# Dibuja una linea sobre los centros de la cuadricula horizontalmente
+def drawPoints(img, w, h, matriz):
+    draw = ImageDraw.Draw(img)
+    for i in range(0, len(matriz)):
+        draw.line(matriz[i],  (255, 215, 0, 255))
+    img.save("editadas/centros.jpg")
 
 # Vuelve la foto blanco y negro sacando los promedios de RGB
 def binarizarImg(img):
     img = img.convert("L") # Convierte la foto a blanco y negro
-    threshold = 110 # Cualquier valor por debajo de este numero sera negro
+    threshold = 120 # Cualquier valor por debajo de este numero sera negro
     img = img.point(lambda p: p > threshold and 255) # Binarizar
-    #img.save("editadas/binarizada.jpg") # Guarda la foto en la carpeta
+    img.save("editadas/binarizada.jpg") # Guarda la foto en la carpeta
     return img
 
 # Genera una subimagen basado en ciertas posiciones
 def cortarImg(img, xMin, yMin, xMax, yMax):
     img = img.crop((xMin, yMin, xMax, yMax))
-    #img.save("editadas/cortada.jpg")
+    img.save("editadas/cortada.png")
     return img
 
+# Dibuja la cuadricula de la foto
 def dibujarCuadricula(img, w, h):
     # Dibujar rayas Verticales
     space = math.trunc(w/7)
     for i in range(1, 7): # Generar 6 lineas
         for y in range(0, h): # Toda la profundidad
-            img.putpixel((space*i, y), (255, 215, 0, 255)) # Lineas en (RGBA)
+            img.putpixel((space * i, y), (255, 215, 0, 255)) # Lineas en (RGBA)
     # Dibujar rayas Horizontales
     space = math.trunc(h/6)
     for i in range(1, 6): # Generar 5 lineas
         for x in range(0, w): # Todo el ancho
             img.putpixel((x, space*i), (255, 215, 0, 255))
-    img.save("editadas/cuadricula.jpg")
+    img.save("editadas/cuadricula.png")
 
+# Saca los estados de la imagen
+def getStates(img, puntos):
+    matrix = []
+    for i in range(0, len(puntos)):
+        aux = []        
+        flagSpace = False
+        for j in reversed(range(0, len(puntos[0]))):
+            c = img.getpixel(puntos[i][j])
+            index = getColor(c)   
+            if not flagSpace:
+                if index == 0:
+                    # Si es Negro
+                    aux.append(-1)
+                elif index == 1:
+                    # Si es Blanco
+                    aux.append(1)
+                elif index == 2:
+                    # Si es Verde
+                    flagSpace = True
+                    aux.append(0)
+            else:
+                aux.append(0)
+        aux.reverse()
+        matrix.append(aux)
+    matrix = np.transpose(matrix)    
+    return matrix
+
+# Pruebas con OpenCV
+def testCV2(img_color): # frame = img
+    #cv2.imshow('Original', img_color)
+    #colores
+    img_r=img_color[:,:,2]
+    img_g=img_color[:,:,1]
+    img_b=img_color[:,:,0]
+    rojo=restarImagen(img_r, img_g)
+    # cv2.imshow('Rojo', img_r)
+    # cv2.imshow('Verde', img_g)
+    # cv2.imshow('Azul', img_b)
+    cv2.imshow('Final', rojo)
+    cv2.waitKey(0)
+
+# Restar dos colores en una img
+def restarImagen(img1, img2):
+    return cv2.subtract(img1, img2)
+
+# Funcion principal que ejecuta todo
 def ejecutar(path):
-    # Cargar la imagen
-    try:  
-        img = Image.open(path)
-    except IOError:
-        print "No se encontro la imagen"
-    # Si la foto cargo exitosamente
-    (w, h) = img.size # Sacar tamanno a la imagen
-    imgB = binarizarImg(img) # Binarizar imagen
-    # Delimitar el tablero
-    (yMin, yMax) = delimitarAlto(imgB, w, h) # Delimitar el tablero verticalmente
-    (xMin, xMax) = delimitarAncho(imgB, w, h) # Delimitar el tablero horizontalmente
-    # Cortar foto
-    imgC = cortarImg(img, xMin,yMin, xMax, yMax)
-    # Dibuja la cuadricula
-    (w, h) = imgC.size # Sacar tamanno a la imagen
-    dibujarCuadricula(imgC, w, h)
+    # Cargar la imagen}
+    try: 
+        # img = Image.open(path)
+        
+        # # Si la foto cargo exitosamente
+        # (w, h) = img.size # Sacar tamanno a la imagen
+        # imgB = binarizarImg(img) # Binarizar imagen
 
-    return True
+        # # Delimitar el tablero
+        # (yMin, yMax) = delimitarAlto(imgB, w, h) # Delimitar el tablero verticalmente
+        # imgB = cortarImg(imgB, 0, yMin, w-1, yMax)
+        # (w, h) = imgB.size
+        # (xMin, xMax) = delimitarAncho(imgB, w, h) # Delimitar el tablero horizontalmente
+
+        # # Cortar foto
+        # imgC = cortarImg(img, xMin, yMin, xMax, yMax)
+
+        # # Dibuja la cuadricula
+        # (w, h) = imgC.size # Sacar tamanno a la imagen
+        # dibujarCuadricula(imgC, w, h)
+
+        # # Saca los centros la cuadricula
+        # centros = getPoints(imgC, w, h)
+
+        # # Sacar los estados de la cuadricula
+        # matrix = getStates(imgC, centros)
+
+        # return matrix
+
+        img = cv2.imread(path)
+        testCV2(img)
+
+
+    except IOError:
+        print("No se encontro la imagen")
