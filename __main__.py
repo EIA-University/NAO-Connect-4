2f77d28debff0784337936519503d18092a6557b
JuanMusic1 <jsvelsaa@hotmail.com>
2019-10-06 22:18:43 -0500

    Final

--- __main__.py
+++ __main__.py
@@ -6,14 +6,17 @@ import Fourinline as games """
 import sensors as todo
 
 #nao.local 9559
- if __name__ == '__nomain__':
+if __name__ == '__nomain__':
     IP = "nao.local"  # Replace here with your NaoQi's IP address.
     PORT = 9559
     todo.main(IP, PORT)
-
+ 
 """ if __name__ == '__main__':
-    path = "imagenes/prueba5.jpg" 
-    #path = "imagenes/pruebaNao1.png" # Las fotos de Nao son .png
-    print path
-    matrix = conv.ejecutar(path)
-    print matrix """
+    import os
+    for file in os.listdir("imagenes/"):
+        if file.endswith(".png"):
+            path =  os.path.join("imagenes/", file)
+            path2 =  os.path.join("cuadricula/", file)
+            imgB = conv.ejecutar(path, path2)
+ """
+
--- sensors.py
+++ sensors.py
@@ -17,8 +17,10 @@ import Fourinline as games
 # Global variable to store the ReactToTouch module instance
 ReactToTouch = None
 memory = None
-IP = None
-PORT = None
+
+primeraJugada = True # Se asume que la primera jugada siempre es del enemigo
+estadoActual = None
+estadoAnterior = None
 
 class ReactToTouch(ALModule):
     """ A simple module able to react
@@ -57,17 +59,110 @@ class ReactToTouch(ALModule):
         # Subscribe again to the event
         memory.subscribeToEvent("TouchChanged", "ReactToTouch", "onTouched")
 
-    
+
+# -------------------Métodos auxiares--------------------------
+
+# Compara el estado actual un estado anterior  de una diferencia
+def aceptarEstado(st1, st2):
+    dif = 0 # Cuenta las diferencias entre los tableros
+    reti = -1
+    retj = -1
+    for i in range(0, len(st1)):
+        for j in range(0, len(st1[0])):
+            if st1[i][j] != st2[i][j]:
+                dif += 1
+                reti = i
+                retj = j
+    # La diferencia solo debe ser de 1 fichas
+    if dif > 1:
+        return (False, reti, retj)
+    return (True, reti, retj)
+
+# Devuelve un estado anterior inicial en caso de ser la primera jugada
+def estadoAnteriorInicial():
+    s =   [[0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],]
+    return s
+
+# Actualiza un estado poniendo una nueva ficha
+def ponerNuevaFicha(estado, jugada):
+    i = 0
+    flag = False # Indica si la ficha ya fue puesta
+    while (i < len(estado) and (not flag)):
+        if estado[i][jugada] != 0:
+            estado[i-1][jugada] = 1 # Se asume que estamos poniendo una jugada de la IA
+            flag = True
+        i += 1
+    if i == len(estado): # Es necesario poner en la ultima fila
+        estado[i-1][jugada] = 1
+    return estado
+
+# Verifica si alguien gano, y retorna quien gano
+def comprobarGanador(estado, i, j):
+    n = -1 # Marca del enemigo
+    operators = [0,1,2,3,4,5,6]
+    m = n
+    br = games.Board(m, i, j, estado, "1",operators=operators, operator=None, parent=None,objective=None)
+    return br.isWinner()
+
+
+
 def jugar(IP, PORT):
+    global primeraJugada, estadoActual, estadoAnterior
+
     path = take.showNaoImage(IP, PORT)
     print path
-    matrix = conv.ejecutar(path)
-    print matrix
-    jugada = games.play(matrix)
-    talk.talk("Put the piece in the colum " + str(jugada), IP, PORT)
-    time.sleep(1)
-    talk.talk("It's your turn, when you finish, touch some sensor for me to play", IP, PORT)
+    primeraJugada = False
+    #estadoActual = conv.ejecutar(path)
+    estadoActual = [[0,0,0,0,0,0,0],
+                    [1,0,0,0,0,0,0],
+                    [1,0,0,0,0,0,0],
+                    [1,0,0,0,0,0,0],
+                    [1,0,0,0,0,0,0],
+                    [1,-1,1,-1,1,1,1],]
+    print estadoActual
 
+    # Revisa si es la primera jugada
+    if primeraJugada:
+        estadoAnterior = estadoAnteriorInicial()
+        (heu, jugada) = games.play(estadoActual) # Esta es la jugada que nos devuelve la IA
+         # Logica
+        estadoAnterior = ponerNuevaFicha(estadoActual, jugada)
+        primeraJugada = False
+        # Hablar
+        talk.talk("Put the piece in the colum " + str(jugada), IP, PORT)
+        time.sleep(1)
+        talk.talk("It's your turn, when you finish, touch me some sensor to play", IP, PORT)
+       
+    else:
+        (acepta, i, j) = aceptarEstado(estadoAnterior, estadoActual)
+        if acepta: # Se puede seguir jugando
+
+            # Verifica si gana el otro
+            ganarOtro = comprobarGanador(estadoActual, i, j)
+
+            if ganarOtro: # Si el otro gana
+                talk.talk("Oh shit, i've lost, teacher, please, forgive us, we are mortals, we aren't perfects", IP, PORT)
+                sys.exit(0)
+            else: # Si nadie gana, juega normal
+                (heu, jugada) = games.play(estadoActual) # Esta es la jugada que nos devuelve la IA
+                estadoAnterior = ponerNuevaFicha(estadoActual, jugada)
+                # Hablar
+                talk.talk("Put the piece in the column " + str(jugada), IP, PORT)
+                # Comprobar si ganamos
+                if heu == 50:
+                    talk.talk("I win, we derserve a 5", IP, PORT)
+                    sys.exit(0)
+                time.sleep(1)
+                talk.talk("It's your turn, when you finish, touch me some sensor to play", IP, PORT)
+                
+
+         else: # Es necesario tomar otra foto
+            talk.talk("It's necesary take another photo", IP, PORT)
 
 
 def main(ip, port):
@@ -97,7 +192,5 @@ def main(ip, port):
         sys.exit(0)
 
 if __name__ == "__main__":
-    IP = "nao.local"  # Replace here with your NaoQi's IP address.
+    IP = "nao.local"
     PORT = 9559
-
-    main(IP, PORT)
